3035f03b6b5356bc03abeae7d7565ebf5ec89983
Ali19Shamlooh <3looy19shamlooh@gmail.com>
2025-10-08 11:18:06 +0300

    ship class and ships array

--- game-logic.js
+++ game-logic.js
@@ -5,27 +5,39 @@ const counterSpan= document.querySelector('#counter')
 //normal vars
 let hitCounter= 0;
 let isWin= false; // winning trigger
+let shipCells= [] // to save the cells that have been a ships
 
 
-
+/////////////////////////////////////////////////
 
 //creates the cells inside each board (divs with class boards)
 function createCells(){
   boards.forEach((board)=>{
-    for(let i =1 ; i <= 100; i++){ // for 100 times
+    for(let i =0 ; i <= 99; i++){ // for 100 times
       const cell = document.createElement("div") //create a cell
       cell.classList.add("cells") //add class "cells" to it
       cell.id = `${i}` // add it's number as an id
       board.appendChild(cell) // add "cell" to DOM inside the div with class "board"
 
       //click event listener
-      cell.addEventListener('click',()=>{
+      cell.addEventListener('click',(cell)=>{
         hitCounter++
         counterSpan.innerText= `tries : ${hitCounter}`
+
+        //
+        cell.classList.forEach((className)=>{
+          if(className == "ship"){//chick if the cell is ship or not
+            cell.innerText='X'
+          }
+        })
+
+
+        checkWin(isWin)
       })
     }
   })
 }
+/////////////////////////////
 
 const checkWin= (isWin)=>{
   if(isWin){
@@ -39,17 +51,45 @@ const checkWin= (isWin)=>{
     //assigning them to the body
     document.body.appendChild(winDiv)
     document.querySelector('.win-div').appendChild(textSpan)
-    console.log('poped')
   }
 }
 
 
+////////////////////////////////////
+
+//check if cell is a ship or not
+//return true if no mach found , false otherwise
+const isNotOccupied = (cell)=>{
+  return shipCells.every((cellID=>{
+    cellID!=cell
+  }))
+}
+
+////////////////////////////////////
+let ships=[]
+class ship{
+  constructor(len){
+    this.len= len
+  }
+}
+const battleship = ship(4); ships.push(battleship)
+const cruiser = ship(3); ships.push(cruiser)
+const destroyer = ship(2); ships.push(destroyer)
+const boat = ship(1); ships.push(boat)
+
+// const ships = [
+//   { name: "battleship", size: 4 },
+//   { name: "cruiser", size: 3 },
+//   { name: "destroyer", size: 2 },
+//   {name:'1', size:1}
+// ];
+//////////////////////////////////
+
+////////////////////////////////////
 
 
 
 //functions execution
 
 createCells()
-placeShipsRandomly();
-
-checkWin(isWin)
+placeShipsRandomly()
